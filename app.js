3197e77f66146165455c2d615d3bff93b3d1b054
Азамат Даулетияров <azamat@MacBook-Air-Azamat.local>
2023-06-12 22:45:43 +0200

    comments added

--- app.js
+++ app.js
@@ -8,19 +8,31 @@ import multer from "multer";
 const forms = multer();
 
 const app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(forms.array())
+
+// Парсинг тела запроса в формате JSON
+app.use(bodyParser.json());
+
+// Парсинг данных формы
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Парсинг данных формы, отправленных через multipart/form-data
+app.use(forms.array());
+
 const PORT = 3000;
 
+// Загрузка переменных окружения из файла .env
 config();
+
+// Подключение к базе данных MongoDB
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.on("open", () => console.log("DB Connected"));
 
+// Маршруты для операций с продавцами
 app.use("/vendor", vendorRouter);
 
+// Запуск сервера
 app.listen(PORT, (error) => {
   if (!error) console.log("Server is Successfully Running, and App is listening on port " + PORT);
   else console.log("Error occurred, server can't start", error);
--- controllers/VendorController.js
+++ controllers/VendorController.js
@@ -2,21 +2,27 @@ import mongoose from "mongoose";
 import { Vendor } from "../models/VendorModel.js";
 import md5 from "md5";
 
+// Получение всех продавцов
 export const getVendors = (req, res) => {
   Vendor.find().then((vendors) => {
     res.send(vendors);
   });
 };
 
+// Создание нового продавца
 export const newVendor = async (req, res) => {
-  let allVendors = await Vendor.find(); // все продавцы
+  let allVendors = await Vendor.find(); // Получение всех продавцов из базы данных
   let vendorFound = false;
+
+  // Проверка, существует ли уже продавец с таким же email
   for (const item of allVendors) {
     if (item.email == req.body.email) {
-      vendorFound = true; // если уже такой вендор есть
+      vendorFound = true; // Если уже есть продавец с таким email
     }
   }
-  if (!vendorFound) { // если вендор не найден
+
+  if (!vendorFound) {
+    // Если продавец не найден, создаем нового продавца
     const vendor = new Vendor({
       email: req.body.email,
       password: md5(req.body.password),
@@ -31,30 +37,36 @@ export const newVendor = async (req, res) => {
         address: req.body.address,
         zip: req.body.zip,
       },
-      vendor_name: req.body.vendor_name
+      vendor_name: req.body.vendor_name,
     });
+
     try {
-      const newVendor = await vendor.save(); // сохраняем данные
-      res.send("V0");
+      const newVendor = await vendor.save(); // Сохранение данных нового продавца в базе данных
+      res.send("V0"); // Отправка клиенту сообщения о успешном создании продавца
     } catch (error) {
       console.log(error);
     }
-  } else { // если уже вендор есть
-    res.send("V41");
+  } else {
+    // Если уже существует продавец с таким email
+    res.send("V41"); // Отправка клиенту сообщения о том, что продавец уже существует
   }
 };
 
+// Авторизация продавца
 export const loginVendor = async (req, res) => {
-  let vendor = await Vendor.find({ email: req.body.email });
+  let vendor = await Vendor.find({ email: req.body.email }); // Поиск продавца по email в базе данных
+
   if (vendor.length == 0) {
-    res.send("V43");
+    // Если продавец не найден
+    res.send("V43"); // Отправка клиенту сообщения о неправильном логине или пароле
   } else {
     vendor = vendor[0];
-    console.log(vendor.password);
+
     if (vendor.password == md5(req.body.password)) {
-      res.send(vendor);
+      // Проверка правильности пароля
+      res.send(vendor); // Отправка клиенту данных авторизованного продавца
     } else {
-      res.send("V43");
+      res.send("V43"); // Отправка клиенту сообщения о неправильном логине или пароле
     }
   }
 };
